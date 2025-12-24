521bc54bc9c2b48285639dbe3ed38c6d5218b0a0
Jens Hoffmann <xmcpam@gmail.com>
2025-12-24 10:36:18 +0100

    Add computer interface

--- 05-xml-based-spring/src/main/java/org/example/Alien.java
+++ 05-xml-based-spring/src/main/java/org/example/Alien.java
@@ -1,7 +1,29 @@
 package org.example;
 
 public class Alien {
+
+  private int age;
+  private Computer computer;
+
+  public Alien() {
+    System.out.println("Alien object created");
+  }
+
   public void code() {
     System.out.println("Coding");
+    computer.compile();
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    System.out.println("setAge called");
+    this.age = age;
+  }
+
+  public void setLaptop(Computer computer) {
+    this.computer = computer;
   }
 }
--- 05-xml-based-spring/src/main/java/org/example/App.java
+++ 05-xml-based-spring/src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ public class App {
   public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     Alien obj = (Alien) context.getBean("alien");
+    System.out.println("Alien is " + obj.getAge() + " years old");
     obj.code();
   }
 }
