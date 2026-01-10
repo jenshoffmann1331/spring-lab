b317f47211e0ab1170dd204bc23e3f77010d683e
Jens Hoffmann <xmcpam@gmail.com>
2026-01-10 12:31:58 +0100

    08 more on jdbc

--- 08-jdbc/08-jdbc/src/main/java/org/example/jdbc/dao/StudentRepository.java
+++ 08-jdbc/08-jdbc/src/main/java/org/example/jdbc/dao/StudentRepository.java
@@ -1,9 +1,11 @@
 package org.example.jdbc.dao;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.example.jdbc.model.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,11 +20,20 @@ public class StudentRepository {
   public void save(Student s) {
     String sql = "insert into student (rollno, name, marks) values (?, ?, ?)";
     int rows = jdbcTemplate.update(sql, s.getRollNo(), s.getName(), s.getMarks());
-    System.out.println(rows + " effected");
+    System.out.println(rows + " affected");
   }
 
+
   public List<Student> findAll() {
-    List<Student> students = new ArrayList<>();
-    return students;
+    String sql = "select * from student";
+    RowMapper<Student> mapper = (rs, _) -> {
+      Student s = new Student();
+      s.setRollNo(rs.getInt("rollno"));
+      s.setName(rs.getString("name"));
+      s.setMarks(rs.getInt("marks"));
+      return s;
+    };
+
+    return jdbcTemplate.query(sql, mapper);
   }
 }
